45bc0010cffc41e7a04278321fbf8ae4480ae2fc
Pyon18Pyon <ayasayshi@gmail.com>
2021-06-17 18:55:49 +1000

    feat: add history icon

--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -6,6 +6,11 @@ function Display() {
   const { number, storedNumber } = useContext(NumberContext);
   return (
     <div className={styles.calculatorDisplay}>
+
+      <div className={styles.history}>
+        <i className={`${styles.fas} fas fa-history`}></i>
+      </div>
+
       <h1 className={styles.displayNumber}>
         {!number.length && !storedNumber ? "0" : number || storedNumber}
       </h1>
